49513c8c4a762534106e8151c16af952cbd75435
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-12-18 01:14:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, send_from_directory
+from flask_compress import Compress
+from flask_cors import CORS
 import os
 
 app = Flask(__name__, static_folder='static/browser')
+
+Compress(app)
+CORS(app)
 
 # Serve the Angular application (index.html)
 @app.route('/')
