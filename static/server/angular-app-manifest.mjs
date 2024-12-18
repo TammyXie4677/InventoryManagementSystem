d5a5844c9dcc80dd6257fdb5c7cdbbc19c8f3c58
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-12-18 16:07:28 -0500

    Update UI

--- static/server/angular-app-manifest.mjs
+++ static/server/angular-app-manifest.mjs
@@ -11,9 +11,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 5799, hash: '85df5968d93d566b4e01354dc892f1993517a771245a40fe933756500b690b42', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1864, hash: '3fb743fa3d294c40768ef5337cddd17d400b174df57be0ce8bd9f1f842d2142b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 5971, hash: 'cc0ef272d66a782ab687cd1651410086334fbed66899311ca26969f69db7a3b3', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
-    'styles-YZ3747Z3.css': {size: 231790, hash: 'oUP/+r4jhn8', text: () => import('./assets-chunks/styles-YZ3747Z3_css.mjs').then(m => m.default)}
+    'index.csr.html': {size: 7005, hash: 'a44e6c7410c7ce4477aee59a3e909bfeaeb2db110d0dd027b2a2d912c7ad7349', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 2839, hash: 'f4fe0e8e85142c241009c82addd10ad2bba730b743a2ad7df097509e60545f6d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 7176, hash: '947626d3ba8501fa8f675fc82496a38a02840ae18ed7abf82751c7d6ac2462ed', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'styles-H4EXOXPV.css': {size: 231768, hash: 'dzbh9X4wu8c', text: () => import('./assets-chunks/styles-H4EXOXPV_css.mjs').then(m => m.default)}
   },
 };
