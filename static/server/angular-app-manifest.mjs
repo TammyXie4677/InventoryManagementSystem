da6c40d88451e7b637a470b1c744aad0ea602116
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-12-20 08:05:58 -0500

    Revert " Add Edit button"
    This reverts commit fbe77d15d61d0e17555e5cd096b734cc54b82c99.

--- static/server/angular-app-manifest.mjs
+++ static/server/angular-app-manifest.mjs
@@ -11,9 +11,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 5538, hash: '82f78de8a0f4064d69b44f93636a54232f00b0148f8d6fae02d2e4ee831d4981', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1637, hash: 'fff856f34dc95fa34dff85aeccb089809a08e0c5edeb4fafa7146ac7dc48c211', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 7671, hash: '11d0ecb4a3efd148bc675c5e111363dc3a39225b23a394017791333ccf59d56d', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5538, hash: 'd5f3d6569d76775782e2271f3fdfa75cdf5cb360b36b3f6aeb0f1fc40d1165c3', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1637, hash: '62b0f578c449e1708cb000e7650a7ac5c3a58d03acc7a826ae41512818ecd26b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 7671, hash: 'e2c919c31921324edda3548e1a04c7c8371fc43c64c73f37cccf0953266f69c1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-H4EXOXPV.css': {size: 231768, hash: 'dzbh9X4wu8c', text: () => import('./assets-chunks/styles-H4EXOXPV_css.mjs').then(m => m.default)}
   },
 };
