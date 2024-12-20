b34844601bc36c44cfd5e3cbc39effe3a7d8ba8e
TammyXie4677 <TammyXie4677@users.noreply.github.com>
2024-12-20 09:31:48 -0500

    Deploy

--- FrontEnd/static/browser/server/angular-app-manifest.mjs
+++ FrontEnd/static/browser/server/angular-app-manifest.mjs
@@ -11,9 +11,9 @@ export default {
   }
 ],
   assets: {
-    'index.csr.html': {size: 5538, hash: '6293ee5e19571503d0d79a75939b271c29622a8fb92ab314b366328629093f05', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1637, hash: '419dea43e092957678fa91f8fe59cb9d39f72f718f716f019c52e66e52c1de61', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 7671, hash: '795c1030d9a7ef9f6670bb81521f5c8f1b34b9dee1568576df778035d5eb25f1', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 5538, hash: '28a8cdde2b7977b7a41f73922dc240eac97f68f2c1933b30d184a977a10f21ac', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1637, hash: 'd7ffd67650c25fc20cec70ff767a772e0f761c0edeeea78ef09d2da8e6831caa', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 7671, hash: '3642eb11d4d0909ffffcc38809cf0888dfb301782297fd751d4659f14149d412', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-H4EXOXPV.css': {size: 231768, hash: 'dzbh9X4wu8c', text: () => import('./assets-chunks/styles-H4EXOXPV_css.mjs').then(m => m.default)}
   },
 };
--- FrontEnd/static/browser/server/assets-chunks/index_server_html.mjs
+++ FrontEnd/static/browser/server/assets-chunks/index_server_html.mjs
@@ -37,7 +37,7 @@ export default `<!doctype html>
   <app-root></app-root>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-WEL2YRR2.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-ZCWZEAVL.js" type="module"></script></body>
 
 </html>
 
